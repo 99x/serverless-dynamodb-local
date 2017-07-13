39ec70ccbc34f63fa00aee3037cb4d728d5c7afc
Lakindu Gunasekara <lakindu1995@gmail.com>
2017-07-13 09:39:21 +0530

    patch 5

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,73 +3,79 @@ const assert = require("chai").assert;
 const http = require ("http");
 const expect = require("chai").expect;
 const should = require("should");
-let aws = require ("aws-sdk");
-
-//Invoke the functions in the index.js
-let port = require("../index.js").port;
-let dynamodbOptions = require("../index.js").dynamodbOptions;
-const migrateHandler = require("../index.js").migrateHandler;
-const seedHanlder = require("../index.js").seedHanlder;
-const removeHandler = require("../index.js").removeHandler;
-const startHandler = require("../index.js").startHandler;
-const endHandler = require("../index.js").endHandler;
-const tables = require("../index.js").tables;
-const seedSources = require("../index.js").seedSources;
-const createTable = require("../index.js").createTable;
-const seed = require("../src/seeder.js");
+const aws = require ("aws-sdk");
+const seeder = require("../src/seeder.js");
 const dataApp = require("../index.js");
 
-//Test cases to check the get port function
-describe("Port",function(){
-  it("Port should return number", function(){    
-    let myport = dataApp.prototype.port;   
+describe("Port function",function(){
+  it("Port should return number",function(){
+    let myport = dataApp.prototype.port;
     assert(typeof myport, "number");
   });
 
-  it("Port value should be >= 0 and < 65536", function () {
-    http.get(dataApp.prototype.port, function (response) {
-     assert.equal(response.statusCode, 200);
-     done();
+  it("Port value should be >= 0 and < 65536",function () {
+  http.get(dataApp.prototype.port, function (response) {
+    assert.equal(response.statusCode, 200);
     });
   });
 });
 
-//Testing the dynamodb options function
-describe("Check the dynamodb function", function(){
-  it("Should listen to endpoint", function () {
+describe("Check the dynamodb function",function(){
+  it("Endpoint should listen to the port",function () {
      let server;
      before(function () {
-       server = dynamodbOptions.listen(port);
-     }); 
+      server = dynamodbOptions.listen(port);
+     });
      after(function () {
       assert.ok;
      });
     });
-  
-  it("Raw should be an object", function(){    
+
+  it("Should be an object",function(){
     let dynamoOptions = dataApp.prototype.dynamodbOptions;
     let raw = new aws.DynamoDB(dynamoOptions);
     raw.should.be.type("object");
   });
-   
-  it("Doc should be an object", function(){    
+  
+  it("Should be an object",function(){
     let dynamoOptions =  dataApp.prototype.dynamodbOptions;
     let doc = new aws.DynamoDB(dynamoOptions);
     doc.should.be.type("object");
   });
 });
-//Testing the seeder.js file write seeds
-describe ("Check the Seeder file", function(){
-  it("Table name should be a string", function(){
-    let tableName1 = seed.writeSeeds.name;
-    expect(tableName1).to.be.a("string");
+
+describe ("Start handler function",function(){
+  it ("Should not  be null",function(){
+    let handler = dataApp.prototype.startHandler;
+    assert(handler =! null);
   });
 });
 
-describe ("Check seedSources function", function(){
-  it ("Seeds should be string",function(){
-    const seed = "testing";
-    let seedTest = dataApp.prototype.seedSources(seed);
-    assert.typeOf(seedTest,"string");    
+describe ("UnmarshalBuffer function",function(){
+   it("Should check the json file", () => {
+    const invoked = seeder.unmarshalBuffer(JSON);
+    const result = JSON.parse.toString();
+    assert.isString(result, "Result is an String");
   });
 });
+
+describe ("createTable functon",function(){
+  it ("Should check as a function",function(){
+    const tbl = dataApp.prototype.createTable;
+    assert.equal(typeof tbl, "function");
+  });
+}); 
+
+describe ("Check the Seeder file",function(){
+  it("Table name shoud be a string",function(){
+    let tblName = seeder.writeSeeds.name;
+    expect(tblName).to.be.a("string");
+  });
+});
+
+describe ("FileExists function", function(){
+  it ("File should be an object", function(){
+    let fileObj = seeder.fileExists(__filename);
+    assert.typeOf(fileObj,"object");  
+  });
+}); 
