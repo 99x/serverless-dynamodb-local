ace1722d6ab076afd7c556603e77308d106429fb
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-10-02 22:40:43 -0300

    fix: change test to check for specific port range

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,71 +1,66 @@
 "use strict";
 //Define the modules required to mocha testing
 const assert = require("chai").assert;
-const http = require ("http");
 const expect = require("chai").expect;
-const should = require("should");
-const aws = require ("aws-sdk");
+const aws = require("aws-sdk");
 const seeder = require("../src/seeder.js");
 const Plugin = require("../index.js");
 
 const serverlessMock = require("./serverlessMock");
 
-describe("Port function",function(){
-  it("Port should return number",function(){
+describe("Port function", function () {
+  it("Port should return number", function () {
     let service = new Plugin(serverlessMock, {});
     assert(typeof service.port, "number");
   });
 
-  it("Port value should be >= 0 and < 65536",function(done){
+  it("Port value should be >= 0 and < 65536", function () {
     let service = new Plugin(serverlessMock, {});
-    http.get(`http://localhost:${service.port}/shell/`, function (response) {
-      assert.equal(response.statusCode, 200);
-      done();
-    });
+    assert(service.port >= 0 && service.port < 65536)
   });
 });
 
-describe("Check the dynamodb function",function(){
-  it("Endpoint should listen to the port",function () {
-     let server;
-     before(function () {
+describe("Check the dynamodb function", function () {
+  it("Endpoint should listen to the port", function () {
+    let server;
+    before(function () {
       server = dynamodbOptions.listen(port);
-     });
-     after(function () {
-      assert.ok;
-     });
     });
+    after(function () {
+      assert.ok;
+    });
+  });
 
-  it("Should be an object",function(){
+  it("Should be an object", function () {
     let dynamoOptions = Plugin.prototype.dynamodbOptions;
     let raw = new aws.DynamoDB(dynamoOptions);
     raw.should.be.type("object");
   });
-  
-  it("Should be an object",function(){
-    let dynamoOptions =  Plugin.prototype.dynamodbOptions;
+
+  it("Should be an object", function () {
+    let dynamoOptions = Plugin.prototype.dynamodbOptions;
     let doc = new aws.DynamoDB(dynamoOptions);
     doc.should.be.type("object");
   });
 });
 
-describe ("Start handler function",function(){
-  it ("Should not  be null",function(){
+describe("Start handler function", function () {
+  it("Should not  be null", function () {
     let handler = Plugin.prototype.startHandler;
-    assert(handler =! null);
+    assert(handler = ! null);
   });
 });
 
 
-describe ("createTable functon",function(){
-  it ("Should check as a function",function(){
+describe("createTable functon", function () {
+  it("Should check as a function", function () {
     const tbl = Plugin.prototype.createTable;
     assert.equal(typeof tbl, "function");
   });
-}); 
+});
 
-describe ("Check the Seeder file",function(){
-  it("Table name shoud be a string",function(){
+describe("Check the Seeder file", function () {
+  it("Table name shoud be a string", function () {
     let tblName = seeder.writeSeeds.name;
     expect(tblName).to.be.a("string");
   });
