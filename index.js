60e535637640853def71c6370351ffbfb57b46e6
Matthias Bianchi <matthias.bianchi.57@gmail.com>
2022-09-05 10:27:24 +0200

    Remove hardcoded region for local use

--- index.js
+++ index.js
@@ -189,7 +189,6 @@ class ServerlessDynamodbLocal {
         } else {
             dynamoOptions = {
                 endpoint: `http://${this.host}:${this.port}`,
-                region: "localhost",
                 accessKeyId: "MOCK_ACCESS_KEY_ID",
                 secretAccessKey: "MOCK_SECRET_ACCESS_KEY",
                 convertEmptyValues: options && options.convertEmptyValues ? options.convertEmptyValues : false,
